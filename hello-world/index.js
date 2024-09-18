6750182c4b5cf51623113935a37589dd39664143
L Shaneel Reddy <shaneelreddy320@gmail.com>
2024-09-18 20:20:51 +0530

    http-server

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,5 +1,18 @@
-const hello = () => {
-    console.log("Hello GitHub!");
-  };
-  
-  hello();
+const fs = require('fs');
+fs.writeFile(
+  "sample.txt",
+  "Hello World. Welcome to Node.js File System module.",
+  (err) => {
+    if (err) throw err;
+    console.log("File created!");
+  }
+);
+fs.readFile("sample.txt", (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
+
+fs.appendFile("sample.txt", " This is my updated content", (err) => {
+  if (err) throw err;
+  console.log("File updated!");
+});
